5281e9fa17101c76503a403e7cc7a850a05bf424
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-01-23 20:26:36 -0500

    stepping simulation

--- simulate.py
+++ simulate.py
@@ -1,4 +1,11 @@
 import pybullet as p   
+import time
 
+# create the GUI
 physicsClient = p.connect(p.GUI)
+# simulation stepper
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(0.0167)
+    print(i)
 p.disconnect()
