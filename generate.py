ffc195a30147c94e15f49006817ee1ba72d16ae9
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-01-29 15:41:04 -0500

    adding a second and third link and joints

--- generate.py
+++ generate.py
@@ -15,7 +15,11 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     [length, width, height] = [1,1,1]
     [x,y,z] = [0,0,0.5]
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length, width, height])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size = [length, width, height])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5], size = [length, width, height])
     pyrosim.End()
 
     return
