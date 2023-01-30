02d3959a8856f9cf1e77e6c93de407cc4ae51da7
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-01-30 16:56:33 -0500

    adding joint 3 and link 4

--- generate.py
+++ generate.py
@@ -20,6 +20,8 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size = [length, width, height])
     pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0,0,1])
     pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5], size = [length, width, height])
+    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0,0.5,0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0], size = [length, width, height])
     pyrosim.End()
 
     return
