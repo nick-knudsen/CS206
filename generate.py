dfc2171fea4a9fd426e6a7c707018289d08e45eb
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-01-30 17:15:33 -0500

    finishing second robot and joints project

--- generate.py
+++ generate.py
@@ -11,7 +11,7 @@ def Create_World():
     return
 
 
-def Create_Robot():
+def Create_Robot_1():
     pyrosim.Start_URDF("body.urdf")
     [length, width, height] = [1,1,1]
     [x,y,z] = [0,0,0.5]
@@ -32,6 +32,16 @@ def Create_Robot():
 
     return
 
-
+def Create_Robot_2():
+    pyrosim.Start_URDF("body.urdf")
+    [length, width, height] = [1,1,1]
+    [x,y,z] = [1.5,0,1.5]
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[length, width, height])
+    pyrosim.End()
+    
 Create_World()
-Create_Robot()
+Create_Robot_2()
