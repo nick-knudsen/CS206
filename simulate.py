ae97b28d8d39f87aa94fc2b74c88d32043372094
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-06 18:26:56 -0500

    adding sensor to BackLeg

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 
@@ -11,8 +12,10 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 # simulation stepper
 for i in range(5000):
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     p.stepSimulation()
     time.sleep(0.01)
     print(i)
