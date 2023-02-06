043a3c0afb9ce6faa4ef72ed847adcc69bef5736
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-06 18:43:46 -0500

    using numpy to store sensor vals

--- simulate.py
+++ simulate.py
@@ -2,6 +2,10 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
+import numpy as np
+
+# constants
+SIMULATION_STEPS = 10000
 
 # create the GUI
 physicsClient = p.connect(p.GUI)
@@ -13,10 +17,11 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = np.zeros(SIMULATION_STEPS)
 # simulation stepper
-for i in range(5000):
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+for i in range(SIMULATION_STEPS):
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     p.stepSimulation()
     time.sleep(0.01)
-    print(i)
 p.disconnect()
