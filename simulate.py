443f76b38e05aa33457446bd3eafceffc29ab8f6
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-20 21:42:36 -0500

    simulation methods

--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ from simulation import SIMULATION
 
 
 simulation = SIMULATION()
-
+simulation.Run()
 # SIMULATION_STEPS = c.SIMULATION_STEPS
 
 # AMPLITUDE_FRONT = c.AMPLITUDE_FRONT
@@ -21,16 +21,6 @@ simulation = SIMULATION()
 # FREQUENCY_BACK = c.FREQUENCY_BACK
 # PHASE_OFFSET_BACK = c.PHASE_OFFSET_BACK
 
-# # create the GUI
-# physicsClient = p.connect(p.GUI)
-# p.setAdditionalSearchPath(pybullet_data.getDataPath())
-# # adding gravity
-# p.setGravity(0,0,-9.8)
-# # adding a floor
-# planeId = p.loadURDF("plane.urdf")
-# robotId = p.loadURDF("body.urdf")
-# p.loadSDF("world.sdf")
-# pyrosim.Prepare_To_Simulate(robotId)
 
 # backLegSensorValues = np.zeros(SIMULATION_STEPS)
 # frontLegSensorValues = np.zeros(SIMULATION_STEPS)
@@ -40,26 +30,7 @@ simulation = SIMULATION()
 # #np.save("data/targetAnglesFront.npy", targetAnglesFront)
 # #np.save("data/targetAnglesBack.npy", targetAnglesBack)
 # #exit()
-# # simulation stepper
-# for i in range(SIMULATION_STEPS):
-#     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-#     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-#     pyrosim.Set_Motor_For_Joint(
-#         bodyIndex = robotId,
-#         jointName = b'Torso_BackLeg',
-#         controlMode = p.POSITION_CONTROL,
-#         targetPosition = targetAnglesBack[i],
-#         maxForce = 500
-#     )
-#     pyrosim.Set_Motor_For_Joint(
-#         bodyIndex = robotId,
-#         jointName = b'Torso_FrontLeg',
-#         controlMode = p.POSITION_CONTROL,
-#         targetPosition = targetAnglesFront[i],
-#         maxForce = 500
-#     )
-#     p.stepSimulation()
-#     time.sleep(0.01)
+
 # np.save("data/backLegSensorValues.npy", backLegSensorValues)
 # np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 # p.disconnect()
--- simulation.py
+++ simulation.py
@@ -1,9 +1,11 @@
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import time
 
 from world import WORLD
 from robot import ROBOT
+import constants as c
 
 
 class SIMULATION:
@@ -18,3 +20,26 @@ class SIMULATION:
         self.robot = ROBOT()
 
         pyrosim.Prepare_To_Simulate(self.robot.robotId)
+
+    def Run(self):
+        # # simulation stepper
+        for i in range(c.SIMULATION_STEPS):
+            print(i)
+            # backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+            # frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+            # pyrosim.Set_Motor_For_Joint(
+            #     bodyIndex = robotId,
+            #     jointName = b'Torso_BackLeg',
+            #     controlMode = p.POSITION_CONTROL,
+            #     targetPosition = targetAnglesBack[i],
+            #     maxForce = 500
+            # )
+            # pyrosim.Set_Motor_For_Joint(
+            #     bodyIndex = robotId,
+            #     jointName = b'Torso_FrontLeg',
+            #     controlMode = p.POSITION_CONTROL,
+            #     targetPosition = targetAnglesFront[i],
+            #     maxForce = 500
+            # )
+            p.stepSimulation()
+            time.sleep(0.01)
