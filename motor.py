3747cccca5490a22dbbf4ff7d53c24d8c915c85f
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-27 17:43:38 -0500

    cleaning up

--- motor.py
+++ motor.py
@@ -8,7 +8,6 @@ from math import pi
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
 
     def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
