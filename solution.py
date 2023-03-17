c8f1007a44775671522367b5f24c7439d080a399
Nick Knudsen <nick.knudsen14@gmail.com>
2023-03-17 14:49:25 -0400

    automating search

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyrosim.pyrosim as pyrosim
+import os
 
 class SOLUTION:
 
@@ -10,6 +11,7 @@ class SOLUTION:
         self.Create_World()
         self.Create_Body()
         self.Create_Brain()
+        os.system("python simulate.py")
 
     def Create_World(self):
         pyrosim.Start_SDF("world.sdf")
