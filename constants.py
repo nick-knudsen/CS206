c948f3875418eaf846cd2e8fc81c451d81cf81cd
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-03-27 14:18:57 -0400

    working on robot urdf

--- constants.py
+++ constants.py
@@ -10,5 +10,8 @@ AMPLITUDE_BACK = pi/16
 FREQUENCY_BACK = 20
 PHASE_OFFSET_BACK = 0
 
-numberOfGenerations = 10
-populationSize = 10
+numberOfGenerations = 1
+populationSize = 1
+
+numSensorNeurons = 3
+numMotorNeurons = 2
--- solution.py
+++ solution.py
@@ -4,10 +4,12 @@ import random
 import os
 import time
 
+import constants as c
+
 class SOLUTION:
 
     def __init__(self, ID):
-        self.weights = 2*np.random.rand(3,2)-1
+        self.weights = 2*np.random.rand(c.numSensorNeurons, c.numMotorNeurons)-1
         self.myID = ID
 
     def Set_ID(self, ID):
@@ -49,13 +51,11 @@ class SOLUTION:
     def Create_Body(self):
         pyrosim.Start_URDF("body.urdf")
         
-        [length, width, height] = [1,1,1]
-        [x,y,z] = [1.5,0,1.5]
-        pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length, width, height])
-        pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1,0,1])
-        pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[length, width, height])
-        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2,0,1])
-        pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[length, width, height])
+        pyrosim.Send_Cube(name="Torso", pos=[0,0,1], size=[1,1,1])
+        pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0,-0.5,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0], size=[0.2,1,0.2])
+        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0,0.5,1])
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0], size=[0.2,1,0.2])
         
         pyrosim.End()
 
